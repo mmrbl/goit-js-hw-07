a31103ed480af2c9cbf9aef447569fc9f29b1cc0
Ihor <itotonk1@gmail.com>
2022-07-27 23:41:50 +0300

    Part1 ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+//деструктуризація об'єкта galleryItems і додавання необхідних атрибутів у розмітку, яка буде створюватися
+
+const createGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  })
+  .join('');
+
+console.log(createGallery);
+
+//додає виществорену розмітку у HTML
+galleryContainer.insertAdjacentHTML('beforeend', createGallery);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  const isPicture = evt.target.classList.contains('.gallery__item');
+  if (isPicture) {
+    return;
+  }
+
+  const largeImage = evt.target.dataset.source;
+
+  //для того, щоб примінився basicLightbox.create в HTML чіплявся скрипт basicLightbox
+
+  const showLargeImage = basicLightbox.create(`
+    <img src="${largeImage}" width="800" height="600">
+`);
+
+  showLargeImage.show();
+}
+
+galleryContainer.addEventListener('click', onImageClick);
+
+// Change code above this line
