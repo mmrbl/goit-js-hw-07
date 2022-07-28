1def1faa8872574a4e2984469596b331c7a6fb4b
Ihor <itotonk1@gmail.com>
2022-07-28 22:24:08 +0300

    Додає рішення другої таски

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+//деструктуризація об'єкта galleryItems і додавання необхідних атрибутів у розмітку, яка буде створюватися
+
+const createGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', createGallery);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
